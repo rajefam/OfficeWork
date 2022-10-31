2785e49b521e6f523080b3f8eb235b185bffa814
rajefam <rajeswarireddy514@gmail.com>
2022-10-31 14:38:52 +0530

    officework

---
+++ src/main/java/com/example/demo/OfficeWork1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeWork1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeWork1Application.class, args);
+	}
+
+}
